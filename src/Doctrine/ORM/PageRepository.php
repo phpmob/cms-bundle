aa6707c9d1d45a430b83e0698b5a2f2703049090
liverbool <nukboon@gmail.com>
2018-07-06 19:30:11 +0700

    fix locale page repository

--- src/Doctrine/ORM/PageRepository.php
+++ src/Doctrine/ORM/PageRepository.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace PhpMob\CmsBundle\Doctrine\ORM;
 
+use Doctrine\ORM\QueryBuilder;
 use PhpMob\CmsBundle\Repository\SlugableRepositoryInterface;
 use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
 use Sylius\Component\Resource\Model\SlugAwareInterface;
@@ -37,5 +38,19 @@ class PageRepository extends EntityRepository implements SlugableRepositoryInter
             ->setParameter(':locale', $locale)
             ->setParameter(':enabled', true)
             ->getQuery()->getOneOrNullResult();
+    }
+
+    /**
+     * @param string $locale
+     *
+     * @return QueryBuilder
+     */
+    public function createLocaledQueryBuilder(string $locale): QueryBuilder
+    {
+        return $this->createQueryBuilder('o')
+            ->join('o.translations', 'translation', 'WITH', 'translation.locale = :locale')
+            ->addSelect('translation')
+            ->setParameter('locale', $locale)
+        ;
     }
 }
